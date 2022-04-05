69d209ec084c42001b36496e90c0c6fdd1d53e00
S1o3urn <d4vid4pplepie@gmail.com>
2022-04-05 14:31:05 -0400

    walls of rooms

--- xtext/examples/assets/halScripts.js
+++
@@ -1,9 +0,0 @@
-(function draw() {
-    var canvas = document.getElementById("roomName")
-    var ctx = canvas.getContext('2d');
-    sensor = new Image();
-    sensor.src = '../images/sensor.jpg';
-    sensor.onload = function() {
-        canvas.drawImage(sensor, 0, 0)
-    }
-}
